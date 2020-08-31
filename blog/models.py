c43fc3e2d43a2d5354d825839e9e41fc0fbe421c
Daniil Kovalev <kovdan01@yandex.ru>
2020-08-31 22:21:20 +0300

    Format

--- blog/models.py
+++ blog/models.py
@@ -1,15 +1,13 @@
 from django.db import models
 from django.utils import timezone
-from django import forms
+
 
 class Post(models.Model):
     author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     text = models.TextField()
-    created_date = models.DateTimeField(
-            default=timezone.now)
-    published_date = models.DateTimeField(
-            blank=True, null=True)
+    created_date = models.DateTimeField(default=timezone.now)
+    published_date = models.DateTimeField(blank=True, null=True)
     tags = models.CharField(max_length=200)
 
     def publish(self):
--- mysite/urls.py
+++ mysite/urls.py
@@ -14,18 +14,19 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.conf.urls import include, url, handler400, handler403, handler404, handler500
+from django.conf.urls import include, url
 from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     url(r'', include('blog.urls')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
+
 handler404 = 'blog.views.error_404_view'
 handler403 = 'blog.views.error_403_view'
 handler400 = 'blog.views.error_400_view'
 handler500 = 'blog.views.error_500_view'
-
